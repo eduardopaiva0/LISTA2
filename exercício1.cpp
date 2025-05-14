854ec6401d2acd2dd614a51513d474047ae1cc5c
eduardopaiva0 <paiva.eduardo@aluno.ifsp.edu.br>
2025-05-14 19:19:31 -0300

    Add files via upload

--- exercício1.cpp
+++ exercício1.cpp
@@ -2,18 +2,16 @@
 using namespace	std;
 
 int main(){
-	double num1, num2, numMaior;
+	double num1, num2;
 	cout << "Digite o primeiro numero: ";
 	cin >> num1;
 	cout << "Digite o segundo numero diferente do primeiro: ";
 	cin >> num2;
 	
 	if(num1 > num2){
-		numMaior = num1;
-		cout << "O maior numero eh o: " << numMaior;
+		cout << "O maior numero eh o: " << num1;
 	}else if(num2 > num1){
-		numMaior = num2;
-		cout << "O maior numero eh o: " << numMaior;
+		cout << "O maior numero eh o: " << num2;
 	}else{
 		cout << "Os dois numeros tem valores iguais";
 	}
