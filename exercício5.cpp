8e0166c0aac27b371525f89d1bf14e23b1cd0818
eduardopaiva0 <paiva.eduardo@aluno.ifsp.edu.br>
2025-05-14 19:41:35 -0300

    Add files via upload

---
+++ exercício5.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace	std;
+
+int main(){
+	double base, altura, area;
+	cout << "Digite a base do retangulo: ";
+	cin >> base;
+	cout << "Digite a altura do retangulo: ";
+	cin >> altura;
+	
+	area = base * altura;
+	
+	if(area > 100){
+		cout << "A area do retangulo eh: " << area << endl;
+		cout << "Terreno grande";
+	}else{
+		cout << "A area do retangulo eh: " << area << endl;
+		cout << "Terreno pequeno;
+	}
+	return 0;
+}
